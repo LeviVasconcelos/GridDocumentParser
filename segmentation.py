e388690e15b338c0811192a55fc440789a40dc5b
Levi Vasconcelos <levivasconcelos@denox.com.br>
2017-04-18 18:41:17 -0300

    UnionMap algorithm completed

--- segmentation.py
+++ segmentation.py
@@ -54,20 +54,22 @@ class Component:
 def createComponent(label, point, outter):
 	c = Component()
 	c.label = label
-	c.points += point
+	c.points += [point]
 	c.outter = outter
 	return c
 
 
 def changeMapComponents(img_map, comp, newComp):
 	for i in comp.points:
-		img_map[tuple(i)] = newcomp
+		img_map[i] = newComp
 
 def fundComponents(comp_dict, img_map, p, eGroups):
-	aux = [ len(com_dict[i].points) for i in eGroups ]
+	aux = [ len(comp_dict[i].points) for i in eGroups ]
 	biggerGroupIdx = aux.index(max(aux))
 	biggerGroup = eGroups[biggerGroupIdx]
-	comp_dict[biggerGroup] += sum(comp_dict[i].points for i in eGroups if i != biggerGroup,[])
+	img_map[p] = biggerGroup
+	comp_dict[biggerGroup].points += [p]
+	comp_dict[biggerGroup].points += sum([comp_dict[i].points for i in eGroups if i != biggerGroup],[])
 
 	for i in eGroups:
 		if i != biggerGroup:
@@ -81,20 +83,20 @@ def mapUnion(img):
 	new_group_idx = 0
 	for i in range(0,img.shape[0]):
 		for j in range(0, img.shape[1]):
+			#raw_input('teste')
+			#print(img_map)
 			eligibleGroups = []
 			outter = None
 
 			for p in getMapNeighbours((i,j), img.shape):
-				if (img[p] == img[(i,j)]):
-					eligibleGroups += img_map[p]
-
+				if (img[p] == img[(i,j)] and img_map[p] not in eligibleGroups):
+					eligibleGroups += [img_map[p]]
 
 			if (len(eligibleGroups) == 0):
 				img_map[(i,j)] = new_group_idx
 				new_group_idx += 1
 				newComp = createComponent(img_map[(i,j)], (i,j), outter )
 				component_dict[img_map[(i,j)]] = newComp
-
 			else:
 				fundComponents(component_dict, img_map, (i,j), eligibleGroups)
 
@@ -115,21 +117,36 @@ img = cv2.imread('BoletoBancario.png', 0)
 _, img = cv2.threshold(img, IMAGE_TH, 255, cv2.THRESH_BINARY)
 p = [0,0]
 
-
 img_dict, img_map = mapUnion(img)
 
+#img_test = np.zeros((10,10), np.uint8)
+#img_test[0:5,5] = 255
+#img_test[0:180,70] = 255
+#img_test[0:90,40:42] = 255
+#img_test[0:30,20:22] = 255
+#img_test[45,0:100] = 255
 
+#img_dict, img_map = mapUnion(img_test)
+#print('max:',np.max(img_map))
+#print('min:',np.min(img_map))
+#print(img_map[40:45,40:45])
+#print(img_test[40:45,40:45])
+#print(len(img_dict))
+#print(img_map[0:5,0:5])
 '''
-c,b,s = floodFill(img, p, img[tuple(p)])
-print(s)
-print(img.shape)
-#print(len(c))
-for p in c:
-	img[tuple(p)] = 100
-cv2.rectangle(img, (s[0][1],s[0][0]), (s[1][1],s[1][0]), 0, 3)
+colord_img = np.zeros([img_test.shape[0], img_test.shape[1], 3], np.uint8)
+colors = randColorVect(np.max(img_map)+2)
+for i in range(0,img_test.shape[0]):
+	for j in range(0,img_test.shape[1]):
+		colord_img[(i,j)] = colors[ img_map[(i,j)] ]
+
+
+cv2.imshow('img_test', colord_img)
+cv2.waitKey(0)
 '''
+
 colord_img = np.zeros([img.shape[0], img.shape[1], 3], np.uint8)
-colors = randColorVect(len(img_dict))
+colors = randColorVect(np.max(img_map)+2)
 for i in range(0,img.shape[0]):
 	for j in range(0,img.shape[1]):
 		colord_img[(i,j)] = colors[ img_map[(i,j)] ]
